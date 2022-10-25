766c88e68011b99b737047de3b3f6360f581d718
RogerMendez19 <106356644+RogerMendez19@users.noreply.github.com>
2022-10-24 21:56:41 -0600

    Add files via upload

---
+++ Ejercicio31.java
@@ -0,0 +1,32 @@
+//De los siguientes cinco números pseudoaleatorios generados por el proceso descrito en pagina 113, con a =12, b = 15 y n = 100, 92 para la semilla
+
+
+/*Next= (a × cur + b) % n----(1)
+Next=(12×92+5)%100
+=(1104+5)%100
+=(1109)%100
+=9
+
+Next= (a × cur + b) % n----(1)
+Next=(12×9+5)%100
+=(108+5)%100
+=(113)%100
+=13
+
+Next= (a × cur + b) % n----(1)
+Next=(12×13+5)%100
+=(156+5)%100=
+(161)%100
+=61
+
+Next= (a × cur + b) % n----(1)
+Next=(12×61+5)%100
+=(732+5)%100
+=(737)%100
+=37
+
+Next= (a × cur + b) % n----(1)
+Next=(12×37+5)%100
+=(444+5)%100
+=(449)%100
+=49*/
